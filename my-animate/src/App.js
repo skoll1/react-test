b125081921bbce5b69e5e0ab6c3fe69da6d2ea3c
ABS_CORP\bl4b <842323422@qq.com>
2018-01-10 11:25:36 +0800

    了解Tween-one的使用

--- my-animate/src/App.js
+++ my-animate/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Demo from './animate/example/a4'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          
+          <Demo />
       </div>
     );
   }
---
+++ my-animate/src/guide.js
@@ -0,0 +1,1 @@
+http://www.css88.com/doc/chrome-devtools/inspect-styles/animations/
